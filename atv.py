fd239873053d2fc867beb3b417ff322a0c8ddbb7
Gabriel Sabino <gabrielleitealtern@gmail.com>
2024-09-13 17:16:12 -0300

    Criado os produtos e a lista

---
+++ atv.py
@@ -0,0 +1,7 @@
+viga_de_aco = {"nome":"Viga de aço" , "preco":35, "qtd":52}
+tijolo = {"nome": "Tijolo" , "preco":50, "qtd":22}
+telha_ceramica = {"nome":"Telha de cerâmica" , "preco":30, "qtd":93}
+argamassa = {"nome":"Argamassa" , "preco":22.5, "qtd":12}
+tinta = {"nome":"Tinta" , "preco":175 , "qtd":43}
+
+produtos = [viga_de_aco,tijolo,telha_ceramica,argamassa,tinta]
